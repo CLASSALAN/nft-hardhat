e3e18dbcbf925446f8d03de97f4417dfc4df6102
Alan <awalan1605@gmail.com>
2023-06-01 10:47:01 +0800

    finish random ipfs nft

--- deploy/02-deploy-random-ipfs-nft.js
+++ deploy/02-deploy-random-ipfs-nft.js
@@ -1,6 +1,32 @@
 const { network } = require("hardhat");
-const { developmentChains } = require("../helper-hardhat-config");
+const { developmentChains, networkConfig } = require("../helper-hardhat-config");
 const { verify } = require("../utils/verify");
+const { storeImages, storeTokenUriMetadata } = require("../utils/uploadToPinata");
+
+const FUND_AMOUNT = "1000000000000000000000";
+const imagesLocation = "./images/randomNft/";
+let tokenUris = [
+    "ipfs://QmaVkBn2tKmjbhphU7eyztbvSQU5EXDdqRyXZtRhSGgJGo",
+    "ipfs://QmYQC5aGZu2PTH8XzbJrbDnvhj3gVs7ya33H9mqUNvST3d",
+    "ipfs://QmZYmH5iDbD6v3U2ixoVAjioSzvWJszDzYdbeCLquGSpVm",
+];
+// const imageUris = [
+//     "ipfs://QmSsYRx3LpDAb1GZQm7zZ1AuHZjfbPkD6J7s9r41xu1mf8",
+//     "ipfs://QmYx6GsYAKnNzZ9A6NvEKV9nf1VaDzJrqDR23Y8YSkebLU",
+//     "ipfs://QmUPjADFGEKmfohdTaNcWhp7VGk26h5jXDA7v3VtTnTLcW",
+// ]
+
+const metadataTemplate = {
+    name: "",
+    description: "",
+    image: "",
+    attributes: [
+        {
+            trait_type: "Cuteness",
+            value: 100,
+        },
+    ],
+};
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments;
@@ -8,6 +34,11 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     const chainId = network.config.chainId;
 
     let vrfCoordinatorV2Address, subscriptionId, vrfCoordinatorV2Mock;
+
+    if (process.env.UPLOAD_TO_PINATA == "true") {
+        console.log("inside upload to pinata.");
+        tokenUris = await handleTokenUris();
+    }
 
     if (chainId == 31337) {
         // create VRFV2 Subscription
@@ -33,10 +64,43 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
         networkConfig[chainId]["callbackGasLimit"],
         tokenUris,
     ];
+    console.log(arguments);
     const randomIpfsNft = await deploy("RandomIpfsNft", {
         from: deployer,
         args: arguments,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     });
+
+    if (chainId == 31337) {
+        await vrfCoordinatorV2Mock.addConsumer(subscriptionId, randomIpfsNft.address);
+    }
+
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...");
+        await verify(randomIpfsNft.address, arguments);
+    }
 };
+
+async function handleTokenUris() {
+    // Check out https://github.com/PatrickAlphaC/nft-mix for a pythonic version of uploading
+    // to the raw IPFS-daemon from https://docs.ipfs.io/how-to/command-line-quick-start/
+    // You could also look at pinata https://www.pinata.cloud/
+    tokenUris = [];
+    const { responses: imageUploadResponses, files } = await storeImages(imagesLocation);
+    for (imageUploadResponseIndex in imageUploadResponses) {
+        let tokenUriMetadata = { ...metadataTemplate };
+        tokenUriMetadata.name = files[imageUploadResponseIndex].replace(".png", "");
+        tokenUriMetadata.description = `An adorable ${tokenUriMetadata.name} pup!`;
+        tokenUriMetadata.image = `ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`;
+        console.log(`Uploading ${tokenUriMetadata.name}...`);
+        const metadataUploadResponse = await storeTokenUriMetadata(tokenUriMetadata);
+        tokenUris.push(`ipfs://${metadataUploadResponse.IpfsHash}`);
+    }
+    console.log("Token URIs uploaded! They are:");
+    console.log(tokenUris);
+    return tokenUris;
+}
+
+module.exports.tags = ["all", "randomipfs", "main"];
